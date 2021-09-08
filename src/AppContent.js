af09ad5400a4c620a29b1fc7959b91eaefe2ceea
cataclysm78 <cataclysm78@yandex.ru>
2021-09-08 21:01:26 +0300

    Добавлен bootstrap

---
+++ src/AppContent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AppComponent extends Component {
+  render() {
+    return (
+      <p>
+        Здесь будет контент!
+        <br />
+        <button className="btn btn-primary" href="#" style={{margin: 5}}>Кнопка</button>
+      </p>
+    )
+  }
+}
